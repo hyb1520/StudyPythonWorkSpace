a743cd306fb24c60bdb2f77a9b4e7323eaabd8ba
hyb1520 <448906513@qq.com>
2016-04-27 23:44:04 +0800

    def function

--- src/hyb/__init__.py
+++ src/hyb/__init__.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8-*-
 
-list1 = [1, 2, 3, 4, 5, 3, 21]
-print(list1)
+def myabs(x):
+    if not isinstance(x,(int,float)):
+        raise TypeError("bad operand type")
+    if x>=0:
+        return x
+    else:
+        return -x
+
+print(myabs(-1))
+print(myabs(9.2))
+# myabs('8')
